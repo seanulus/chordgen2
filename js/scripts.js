2ef87d173bde83ca6e179ddc3e6942838a870682
jbs <soffer94@gmail.com>
2018-03-19 20:42:27 -0700

    Update logic.

--- js/scripts.js
+++ js/scripts.js
@@ -13,60 +13,70 @@ function noteMachine(noteInput) {
 }
 
 function scaleMachine(chromaticNotes, scaleInput) {
-    var newScale = [];
-    if (scaleInput === "Major") {
-      for (var i = 0; i < 12; i += 1) {
-        if (i === 1 || i === 3 || i === 6 || i === 8 || i === 10) { continue; }
-          newScale.push(chromaticNotes[i]);
-        } return newScale;
-      }
-    else if (scaleInput === "Dorian") {
-      for (var i = 0; i < 12; i += 1) {
-        if (i === 1 || i === 4 || i === 6 || i === 8 || i === 11) { continue; }
-          newScale.push(chromaticNotes[i]);
-        } return newScale;
+  var newScale = [];
+  if (scaleInput === "Major") {
+    for (var i = 0; i < 12; i += 1) {
+      if (i === 1 || i === 3 || i === 6 || i === 8 || i === 10) { continue; }
+        newScale.push(chromaticNotes[i]);
+      } return newScale;
     }
-    else if (scaleInput === "Phrygian") {
-      for (var i = 0; i < 12; i += 1) {
-        if (i === 2 || i === 4 || i === 6 || i === 9 || i === 11) { continue; }
-          newScale.push(chromaticNotes[i]);
-        } return newScale;
-    }
-    else if (scaleInput === "Lydian") {
-      for (var i = 0; i < 12; i += 1) {
-        if (i === 1 || i === 3 || i === 5 || i === 8 || i === 10) { continue; }
-          newScale.push(chromaticNotes[i]);
-        } return newScale;
-    }
-    else if (scaleInput === "Mixolydian") {
-      for (var i = 0; i < 12; i += 1) {
-        if (i === 1 || i === 3 || i === 6 || i === 8 || i === 11) { continue; }
-          newScale.push(chromaticNotes[i]);
-        } return newScale;
-    }
-    else if (scaleInput === "Minor") {
-      for (var i = 0; i < 12; i += 1) {
-        if (i === 1 || i === 4 || i === 6 || i === 9 || i === 11) { continue; }
-          newScale.push(chromaticNotes[i]);
-        } return newScale;
-    }
-    else if (scaleInput === "Locrian") {
-      for (var i = 0; i < 12; i += 1) {
-        if (i === 2 || i === 4 || i === 7 || i === 9 || i === 11) { continue; }
-          newScale.push(chromaticNotes[i]);
-        } return newScale;
+  else if (scaleInput === "Dorian") {
+    for (var i = 0; i < 12; i += 1) {
+      if (i === 1 || i === 4 || i === 6 || i === 8 || i === 11) { continue; }
+        newScale.push(chromaticNotes[i]);
+      } return newScale;
+  }
+  else if (scaleInput === "Phrygian") {
+    for (var i = 0; i < 12; i += 1) {
+      if (i === 2 || i === 4 || i === 6 || i === 9 || i === 11) { continue; }
+        newScale.push(chromaticNotes[i]);
+      } return newScale;
+  }
+  else if (scaleInput === "Lydian") {
+    for (var i = 0; i < 12; i += 1) {
+      if (i === 1 || i === 3 || i === 5 || i === 8 || i === 10) { continue; }
+        newScale.push(chromaticNotes[i]);
+      } return newScale;
+  }
+  else if (scaleInput === "Mixolydian") {
+    for (var i = 0; i < 12; i += 1) {
+      if (i === 1 || i === 3 || i === 6 || i === 8 || i === 11) { continue; }
+        newScale.push(chromaticNotes[i]);
+      } return newScale;
+  }
+  else if (scaleInput === "Minor") {
+    for (var i = 0; i < 12; i += 1) {
+      if (i === 1 || i === 4 || i === 6 || i === 9 || i === 11) { continue; }
+        newScale.push(chromaticNotes[i]);
+      } return newScale;
+  }
+  else if (scaleInput === "Locrian") {
+    for (var i = 0; i < 12; i += 1) {
+      if (i === 2 || i === 4 || i === 7 || i === 9 || i === 11) { continue; }
+        newScale.push(chromaticNotes[i]);
+      } return newScale;
+  }
+}
+
+function chordMachine(newScale, scaleInput) {
+  var i = { chords: ["maj", "maj7", "maj9", "maj11", "maj13"] }
+  var ii = ["min", "min7", "min9", "min11", "min13"]
+  var iii = ["min", "min7", "min7(b9)", "min11(b9)", "min11 (b9, b13)"]
+  var iv = ["maj", "maj7", "maj9", "maj9(#11)", "maj13(#11)"]
+  var v = ["maj", "7", "9", "11", "13"]
+  var vi = ["min", "min7", "min9", "min11", "min11(b13)"]
+  var vii = ["dim", "min7(b5)", "min7(b5, b9)", "min11(b5, b9)", "min11(b5, b9, b13)"]
+  var newObject = [];
+  if (scaleInput === "Major") {
+    for (var i = 0; i < 7; i += 1) {
+      newObject.push(newScale[0] + i.chords)
     }
   }
 
+}
 
 
-  //   for (i = 0; i < 10; i++) {
-  //       if (i === 1 ) { break; } // USE THIS FOR SCALES!
-  //       text += "The number is " + i + "<br>";
-  //   }
-  //
-  //   }
-  // }
+
   // var chords = {
   //   majI: ["maj"],
   //   majII: ["min"],
